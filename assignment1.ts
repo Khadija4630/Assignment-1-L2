08a927a470415c3548755aed729aebefee083c7b
Khadija4630 <khadijahaque085@gmail.com>
2025-05-08 12:28:49 -0400

    problem solving

--- assignment1.ts
+++ assignment1.ts
@@ -4,58 +4,60 @@
             return input.toLowerCase();
         }
         return input.toUpperCase();
-        };
+        }; 
 
-      function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]
+    function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]
         {
-            return items.filter(item => item.rating > 4);
-     };
+            return items.filter(item => 
+                item.rating >= 4
+            );
+    };
 
 
     function concatenateArrays<T>(...arrays: T[][]): T[] {
         return arrays.flat();
     };
 
-class Vehicle {
-    constructor(private make: string, private year: number) {}
+    class Vehicle {
+        constructor(private make: string, private year: number) {};
 
-getInfo (): string {
-    return `Make: ${this.make}, Year: ${this.year}`;
-}
-};
+        getInfo (): string {
+            return `Make: ${this.make}, Year: ${this.year}`;
+        };
+    };
 
-class Car extends Vehicle {
-constructor(make: string, year: number, private model: string) {
-    super(make, year);   
-}
-getModel (): string {
-    return  `Model: ${this.model}`;
-}
-};
-
-function processValue(value: string | number): number {
-    if (typeof value === "string") {
-        return value.length;
-    } else {
-        return value * 2;
+    class Car extends Vehicle {
+        constructor(make: string, year: number, private model: string) {
+        super(make, year);   
+    };
+    getModel (): string {
+        return  `Model: ${this.model}`;
     }
 };
 
-interface Product {
-    name: string;
-    price: number;
-  };
-  
-  function getMostExpensiveProduct(products: Product[]): Product | null
+    function processValue(value: string | number): number {
+        if (typeof value === "string") {
+            return value.length;
+        } else {
+           return value * 2;
+        };
+    };
+
+    interface Product {
+        name: string;
+        price: number;
+    };
+
+    function getMostExpensiveProduct(products: Product[]): Product | null
     {
         if (products.length === 0) {
-        return null;
+            return null;
         }
         return products.reduce((max, product) => 
             product.price > max.price ? product : max
     ); 
-    };
-   
+};
+
     enum Day {
         Monday,
         Tuesday,
